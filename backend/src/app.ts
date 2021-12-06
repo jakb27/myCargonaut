e134132c9c2b59d26161a82a469b70749ce00e0a
Jamah Akbary <jamah.akbary@mni.thm.de>
2021-12-06 10:49:04 +0100

    Server to heroku

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import express,{Application, Request, Response, NextFunction} from 'express';
 const app: Application = express();
 
 app.get('/',(req: Request, res: Response, next: NextFunction) => {
-    res.send('This is the Base');
+    res.send('This is the SERVER');
 });
 
-app.listen(5000,() => console.log('Server running'));
+app.listen(process.env.PORT || 3000);
