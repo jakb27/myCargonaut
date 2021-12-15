0482072e97043600fe3be9b58f751db283c76f27
Tim Eggers <tim.luca.eggers@mni.thm.de>
2021-12-15 12:09:08 +0100

    footer fix

--- myCargonaut/src/app/components/case-list/case-list.component.ts
+++ myCargonaut/src/app/components/case-list/case-list.component.ts
@@ -50,15 +50,5 @@ export class CaseListComponent implements OnInit {
     this.showOwn = !this.showOwn;
   }
 
-  public timeConverter(UNIX_timestamp: any){
-    let a = new Date(UNIX_timestamp * 1000);
-    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    let year = a.getFullYear();
-    let month = months[a.getMonth()];
-    let date = a.getDate();
-    let hour = a.getHours();
-    let min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
-    let sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
-    return date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-  }
+
 }
--- myCargonaut/src/app/shared/services/case.service.ts
+++ myCargonaut/src/app/shared/services/case.service.ts
@@ -40,4 +40,16 @@ export class CaseService {
     console.log(c.id)
     return this.fs.doc('cases/' + c.id).delete();
   }
+
+  public timeConverter(UNIX_timestamp: any){
+    let a = new Date(UNIX_timestamp * 1000);
+    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+    let sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
+    return date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  }
 }
